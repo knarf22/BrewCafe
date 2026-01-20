17d4c419ed48eb112f22e95871523351da977b97
knarf22 <gonzalesjosefrank22@gmail.com>
2026-01-20 23:16:30 +0800

    remove hard coded date

--- BrewCafe/Services/Implementations/DateTimeProvider.cs
+++ BrewCafe/Services/Implementations/DateTimeProvider.cs
@@ -4,8 +4,6 @@ namespace BrewCafe.Services.Implementations
 {
     public class DateTimeProvider : IDateTimeProvider
     {
-        //public DateTime Now => new DateTime(2026, 4, 1);
-
         public DateTime Now => DateTime.Now;
     }
 }
